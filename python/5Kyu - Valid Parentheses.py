adc09041ad697f49669a7a4681c51749d50e57bf
Greg Hodgson <Ghodgso1@gmail.com>
2018-07-15 22:58:42 -0400

    Create 5Kyu - Valid Parentheses.py

---
+++ python/5Kyu - Valid Parentheses.py
@@ -0,0 +1,17 @@
+# My Solution
+
+def valid_parentheses(string):
+    stack = []
+    for s in string:
+        if(s == '('):
+            stack.append(s)
+        elif(s == ')'):
+            try:
+                stack.pop()
+            except:
+                return False
+
+    if(len(stack) == 0):
+        return True
+    else:
+        return False

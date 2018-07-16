d3f6b238f8fe751a06c24cc63f65b19b3d5d3950
Greg Hodgson <Ghodgso1@gmail.com>
2018-07-15 21:53:14 -0400

    Create 7Kyu - SQL Basics: Maths with String Manipulations.sql

---
+++ SQL/7Kyu - SQL Basics: Maths with String Manipulations.sql
@@ -0,0 +1,17 @@
+"""
+Given a demographics table in the following format:
+
+demographics table schema
+
+id
+name
+birthday
+race
+return a single column named 'calculation' where the value is the bit length of name, added to the number of characters in race.
+"""
+
+
+# My Solution
+
+SELECT BIT_LENGTH(name)+LENGTH(race) AS calculation
+FROM demographics;

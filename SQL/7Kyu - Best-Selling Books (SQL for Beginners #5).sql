a5185ca6b7c4f5b864fc9c06206f1440fb476b2c
Greg Hodgson <Ghodgso1@gmail.com>
2018-07-15 21:52:09 -0400

    Create 7Kyu - Best-Selling Books (SQL for Beginners #5).sql

---
+++ SQL/7Kyu - Best-Selling Books (SQL for Beginners #5).sql
@@ -0,0 +1,17 @@
+"""
+You work at a book store. It's the end of the month, and you need to find out the 5 bestselling books at your store. Use a select statement to list names, authors, and number of copies sold of the 5 books which were sold most.
+
+books table schema
+
+name
+author
+copies_sold
+"""
+
+
+# My Solution
+
+SELECT name, author, copies_sold
+FROM books
+ORDER BY copies_sold
+DESC LIMIT 5
